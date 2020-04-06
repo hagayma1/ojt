be1b3cff5f1edeb827a78e4b390b1653213d7dd8
mordicool <mordicool@gmail.com>
2020-04-06 20:52:17 +0300

    Some CR notes

--- todo-list/src/app/app.component.ts
+++ todo-list/src/app/app.component.ts
@@ -4,37 +4,36 @@ import backupSaver from './modules/backupSaver';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-
 export class AppComponent {
   title = 'todo-list';
-  tasks = backupSaver.restore("tasks");
+  tasks = backupSaver.restore('tasks'); // CR: "tasks" should be a GLOBAL CONSTANT (file scope/project scope)
   tasksToShow = this.tasks;
-  filter = "";
+  filter = '';
 
   constructor() {
-    if(this.tasks == null) {
+    if (this.tasks == null) {
       this.tasks = [];
       this.tasksToShow = [];
-    }  
+    }
   }
 
-  onTaskAdded(data: { text: string, checked: boolean }) {
-    console.log(this.tasks);
+  onTaskAdded(data: { text: string; checked: boolean }) {
+    console.log(this.tasks); // CR: always remove all console.log calls
     this.tasks.push(data);
     this.filterTasks();
     this.backup();
   }
 
-  onUpdateTask(data: { text: string, checked: boolean }) {
-    let indexToUpdate = this.tasks.findIndex(item => item.text === data.text);
+  onUpdateTask(data: { text: string; checked: boolean }) {
+    let indexToUpdate = this.tasks.findIndex((item) => item.text === data.text);
     this.tasks[indexToUpdate].checked = data.checked;
     this.filterTasks();
     this.backup();
   }
 
-  onDeleteTask(data: { text: string, checked: boolean }) {
+  onDeleteTask(data: { text: string; checked: boolean }) {
     let indexToDelete = this.tasks.indexOf(data);
     this.tasks.splice(indexToDelete, 1);
     this.filterTasks();
@@ -47,10 +46,12 @@ export class AppComponent {
   }
 
   filterTasks() {
-    this.tasksToShow = this.tasks.filter(task => task.text.includes(this.filter));
+    this.tasksToShow = this.tasks.filter((task) =>
+      task.text.includes(this.filter)
+    );
   }
 
   backup(): void {
-    backupSaver.save("tasks", this.tasks);
+    backupSaver.save('tasks', this.tasks);
   }
 }
--- todo-list/src/app/modules/backupSaver.ts
+++ todo-list/src/app/modules/backupSaver.ts
@@ -1,12 +1,10 @@
 class backupSaver {
-    
-    save(key, data):void {
-        localStorage.setItem(key,JSON.stringify(data));
-    }
+  save(key, data): void {
+    localStorage.setItem(key, JSON.stringify(data));
+  }
 
-    restore(key) {
-        return eval(localStorage.getItem(key));
-    }
-
+  restore(key) {
+    return eval(localStorage.getItem(key)); // CR: why eval?!
+  }
 }
 export default new backupSaver();
